87e686a7d94a3e3113b2468c7ac2b62745b41139
seifeddinesaad <saadseif1925@outlook.com>
2023-11-28 09:52:11 +0000

    fixing a bug while runing the server

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -19,7 +19,7 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args) => {
+export const getPayloadClient = async ({ initOptions }: Args={}) => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing!");
   }
