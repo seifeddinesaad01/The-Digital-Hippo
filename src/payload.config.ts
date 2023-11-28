93832aadf70c81fcae8014a9abe3a7a07580cb96
seifeddinesaad <saadseif1925@outlook.com>
2023-11-28 09:04:28 +0000

    setting up,our admin dashboard

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,30 @@
-// import {buildConfig} from "payload/config"
-
-// export default buildConfig({
-//     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-//     collections:[],
-//     routes:{
-//         admin:'/sell'
-//     },
-//     admin:{},
-//     editor:slateEditor({}),
-
-// }) 
+import { buildConfig } from "payload/config";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import path from "path";
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: "- DigitakHippo",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
+    },
+  },
+  rateLimit: {
+    max: 200,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript:{
+    outputFile: path.resolve(__dirname, 'payload-types.ts')
+  }
+});
