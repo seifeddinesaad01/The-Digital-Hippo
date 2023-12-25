3c090a824305ab22b66e55adc4283131a3cf284d
seifeddinesaad <saadseif1925@outlook.com>
2023-12-25 18:58:16 +0000

    Updating the content of the README file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# The Digital Hippo 
-![Screenshot](./public/thumbnail.jpg")
+# The digital Hippo
+![The Digital Hippo](./public/digitalHippoImages/a.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/asss.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/b.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/c.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/d.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/e.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/f.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/k.jpg "")
+![The Digital Hippo](./public/digitalHippoImages/m.jpg "")
+
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from 'next/image'
- import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import type SwiperType from 'swiper'
@@ -37,8 +37,7 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
 
   const activeStyles =
     'active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300'
-  const inactiveStyles = 'hidden text-gray-400'
-
+  const inactiveStyles = 'hidden text-gray-400';
   return (
     <div className='group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl'>
       <div className='absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition'>
